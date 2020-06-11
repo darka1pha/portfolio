6c478414f0a011aeb2f7629b1a016760773a2ab3
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2020-06-11 23:29:49 +0430

    add md screen view

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
         setShowMenu(!showMenu)
     }
 
+    const [active , setActive] = useState('Home')
+
     return (
         <div className='header'>
             <div onClick={onMenuClick} className='menu-btn'>
@@ -16,16 +18,16 @@ const Navbar = () => {
             <nav className={`nav  ${showMenu ? 'open' : ''}`}>
                 <ul className={`menu-nav ${showMenu ? 'open' : ''}`}>
                     <li className={`menu-nav__item ${showMenu ? 'open' : ''}`}>
-                        <Link to='/' className="menu-nav__link active">HOME</Link>
+                        <Link onClick={() => setActive('Home')} to='/' className={`menu-nav__link ${active === 'Home' ? 'active' : ''}`}>HOME</Link>
                     </li>
                     <li className={`menu-nav__item ${showMenu ? 'open' : ''}`}>
-                        <Link to='/about' className="menu-nav__link">ABOUT ME</Link>
+                        <Link onClick={() => setActive('About')} to='/about' className={`menu-nav__link ${active === 'About' ? 'active' : ''}`}>ABOUT ME</Link>
                     </li>
                     <li className={`menu-nav__item ${showMenu ? 'open' : ''}`}>
-                        <Link to='/projects' className="menu-nav__link">MY PROJECTS</Link>
+                        <Link onClick={() => setActive('Projects')} to='/projects' className={`menu-nav__link ${active === 'Projects' ? 'active' : ''}`}>MY PROJECTS</Link>
                     </li>
                     <li className={`menu-nav__item ${showMenu ? 'open' : ''}`}>
-                        <Link to='/contact' className="menu-nav__link">CONTACT ME</Link>
+                        <Link onClick={() => setActive('Contact')} to='/contact' className={`menu-nav__link ${active === 'Contact' ? 'active' : ''}`}>CONTACT ME</Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -4,7 +4,7 @@ import './home.styles.scss'
 const Home = () => {
     return (
         <div class='home'>
-            <h2>Hi! my name is</h2>
+            <h2 className='home__first'>Hi! my name is</h2>
             <h1 className="home__name">
                 Abolfazl 
                 <span className="home__name--last">
@@ -12,7 +12,7 @@ const Home = () => {
                 </span>
             </h1>
             <h2>
-                Web Developer , Engenieer & Programmer
+                Web Developer , Engineer & Programmer
             </h2>
         </div>
     )
